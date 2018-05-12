8ad4b217a5604154deef7cf33fd7898cf4e84151
oxleberry <oxleberry@gmail.com>
2018-05-12 12:34:53 -0700

    refined logic to pull images specific to link

--- my-webstore/js/app.js
+++ my-webstore/js/app.js
@@ -1,35 +1,37 @@
 
 /* VARIABLES ========================================= */
 var homelinkEl = document.getElementsByTagName('a')[0];
-var storeContainerEl = document.getElementById('store-container');
 var storeLinksEls = document.getElementsByClassName('store-link');
-var storeImgsEls = document.getElementsByClassName('store-img');
+var storeContainerEl = document.getElementById('store-container');
 
 
 /* EVENTS ========================================= */
 
 // show webstore images, when clicked on a link
 for (var i = 0; i < storeLinksEls.length; i++){
+
     storeLinksEls[i].addEventListener('click', function(){
+        // grabs data-name from link to find the right picture
+        var linkData = this.dataset.name;
+        // console.log(linkData);
+        storeContainerEl.innerHTML = "";
         storeContainerEl.className = 'show';
-        findImages ();
+        findImages(linkData);
     });
 }
 
+// loads images based on the linkData
+function findImages(linkData){
+    for (var i = 0; i < 5; i++){
+        var itemName = `images/${linkData}_${i}.png`;
+        var imgEl = document.createElement('img');
+        imgEl.setAttribute('src', itemName);
+        imgEl.className = 'store-img';
+        storeContainerEl.appendChild(imgEl);
+    }
+}
 
 // hide webstore images, when clicked back to HOME link
 homelinkEl.addEventListener('click', function(){
     storeContainerEl.className = 'hide';
 });
-
-
-// loads images
-function findImages (){
-    for (var i = 0; i < storeImgsEls.length; i++){
-        var itemName = `images/oxle_${i}.jpg`;
-        var imgEl = document.createElement('img');
-        imgEl.setAttribute('src', itemName);
-        storeImgsEls[i].appendChild(imgEl);
-        console.log('hi');
-    }
-}
