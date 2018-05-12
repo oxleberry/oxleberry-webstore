8d3e84f7f5f2fa5325ce1e7dbc0be16016cddd88
oxleberry <oxleberry@gmail.com>
2018-05-12 15:17:57 -0700

    refactored to pull data from objects in item.js file

--- my-webstore/js/app.js
+++ my-webstore/js/app.js
@@ -3,31 +3,62 @@
 var homelinkEl = document.getElementsByTagName('a')[0];
 var storeLinksEls = document.getElementsByClassName('store-link');
 var storeContainerEl = document.getElementById('store-container');
-
+var divEl = document.createElement('div');
 
 /* EVENTS ========================================= */
 
 // show webstore images, when clicked on a link
 for (var i = 0; i < storeLinksEls.length; i++){
-
     storeLinksEls[i].addEventListener('click', function(){
-        // grabs data-name from link to find the right picture
+        // grabs data-name from link to find the right pictures
         var linkData = this.dataset.name;
-        // console.log(linkData);
+        // clears content and show new store brand
         storeContainerEl.innerHTML = "";
         storeContainerEl.className = 'show';
-        findImages(linkData);
+        divEl.className = 'container';
+        var storeObj = whichBrand(linkData);
+        // find images and applies to the div
+        findImages(storeObj);
+        storeContainerEl.appendChild(divEl);
     });
 }
 
-// loads images based on the linkData
-function findImages(linkData){
-    for (var i = 0; i < 5; i++){
-        var itemName = `images/${linkData}_${i}.png`;
+// determines from list of store Brand Names
+function whichBrand(linkData) {
+    if (linkData === 'oxle') {
+        return oxleItemsObj;
+    }
+    else if (linkData === 'bash') {
+        return bashItemsObj;
+    }
+    else if (linkData === 'dino') {
+        return dinoItemsObj;
+    }
+}
+
+
+// loads name, price, and img based on objects in ITEMS.js
+function findImages(storeObj){
+    divEl.innerHTML = "";
+    for (var i = 0; i < storeObj.length; i++){
+		var currentItem = storeObj[i];
+        //creates containers for elements
+        var divEl2 = document.createElement('div');
+        divEl2.className = 'img-container';
+        divEl.appendChild(divEl2);
+        // creates the Name Title and Price
+        var h3El = document.createElement('h3');
+        var h4El = document.createElement('h4');
+        h3El.textContent = currentItem.name;
+        h4El.textContent = `$${currentItem.price}`;
+        // finds the Images
         var imgEl = document.createElement('img');
-        imgEl.setAttribute('src', itemName);
+        imgEl.setAttribute('src', currentItem.img);
         imgEl.className = 'store-img';
-        storeContainerEl.appendChild(imgEl);
+        // appends name, price, and image
+        divEl2.appendChild(h3El);
+        divEl2.appendChild(h4El);
+        divEl2.appendChild(imgEl);
     }
 }
 
---
+++ my-webstore/js/items.js
@@ -0,0 +1,88 @@
+
+
+// Oxleberry Store
+var oxleItemsObj = [
+	{
+    	name: 'Monster Buddies',
+		img: 'images/oxle_0.png',
+        price: 15
+	},
+    {
+    	name: 'Hot Rod Bunny',
+		img: 'images/oxle_1.png',
+        price: 15
+	},
+	{
+		name: 'Narwhal and Me',
+		img: 'images/oxle_2.png',
+        price: 15
+	},
+	{
+		name: 'Octopus and Me',
+		img: 'images/oxle_3.png',
+        price: 15
+	},
+	{
+		name: 'Wyatt',
+		img: 'images/oxle_4.png',
+        price: 15
+	}
+];
+
+// Bash and Sass Store
+var bashItemsObj = [
+	{
+    	name: 'Babu on Black',
+		img: 'images/bash_0.png',
+        price: 35
+	},
+    {
+    	name: 'Babu on Grey',
+		img: 'images/bash_1.png',
+        price: 35
+	},
+	{
+		name: 'Breeze on Black Hoodie',
+		img: 'images/bash_2.png',
+        price: 35
+	},
+	{
+		name: 'Breeze on Longsleeve',
+		img: 'images/bash_3.png',
+        price: 35
+	},
+	{
+		name: 'Breeze on Grey Hoodie',
+		img: 'images/bash_4.png',
+        price: 35
+	}
+];
+
+// DINOSAUROUS Store
+var dinoItemsObj = [
+	{
+    	name: 'Dino Party',
+		img: 'images/dino_0.png',
+        price: 25
+	},
+    {
+    	name: 'Dino Allover',
+		img: 'images/dino_1.png',
+        price: 25
+	},
+	{
+		name: 'Dino Glow',
+		img: 'images/dino_2.png',
+        price: 25
+	},
+	{
+		name: 'Dino Blue',
+		img: 'images/dino_3.png',
+        price: 25
+	},
+	{
+		name: 'Dino Orange',
+		img: 'images/dino_4.png',
+        price: 25
+	}
+];
