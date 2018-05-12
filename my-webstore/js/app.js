10f5be2d94caa8cc1ce04fb1726ec185c3fdf816
oxleberry <oxleberry@gmail.com>
2018-05-12 10:21:05 -0700

    loads images when a store link is clicked

--- my-webstore/js/app.js
+++ my-webstore/js/app.js
@@ -1,21 +1,35 @@
 
 /* VARIABLES ========================================= */
-var homeEl = document.getElementsByTagName('a')[0];
+var homelinkEl = document.getElementsByTagName('a')[0];
 var storeContainerEl = document.getElementById('store-container');
-var linksEl = document.getElementsByClassName('link');
+var storeLinksEls = document.getElementsByClassName('store-link');
+var storeImgsEls = document.getElementsByClassName('store-img');
 
 
 /* EVENTS ========================================= */
 
-
 // show webstore images, when clicked on a link
-for (var i = 0; i < linksEl.length; i++){
-    linksEl[i].addEventListener('click', function(){
+for (var i = 0; i < storeLinksEls.length; i++){
+    storeLinksEls[i].addEventListener('click', function(){
         storeContainerEl.className = 'show';
+        findImages ();
     });
 }
 
+
 // hide webstore images, when clicked back to HOME link
-homeEl.addEventListener('click', function(){
+homelinkEl.addEventListener('click', function(){
     storeContainerEl.className = 'hide';
 });
+
+
+// loads images
+function findImages (){
+    for (var i = 0; i < storeImgsEls.length; i++){
+        var itemName = `images/oxle_${i}.jpg`;
+        var imgEl = document.createElement('img');
+        imgEl.setAttribute('src', itemName);
+        storeImgsEls[i].appendChild(imgEl);
+        console.log('hi');
+    }
+}
